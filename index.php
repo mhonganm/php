1a61c6d88381a2fb5df622bc1e9de1dfd36e1afa
Mikko Honganmäki <42143818+mhonganm@users.noreply.github.com>
2018-10-05 21:08:11 +0300

    janne
    janne

--- index.php
+++ index.php
@@ -11,5 +11,6 @@
 <?php echo '<p>Lisätty rivi 25.9</p>'; ?>
 <?php echo '<p>Juhan Demo</p>'; ?>
 <?php echo '<p>Jenkins demo</p>'; ?>
+<?php echo '<p>Terve janne, mitä kuuluu?</p>'; ?>
 </body>
 </html>
