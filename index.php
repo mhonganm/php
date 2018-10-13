ea469606e2014cde61f5cec4c29d81cb2d362807
Mikko Honganmäki <42143818+mhonganm@users.noreply.github.com>
2018-10-14 02:13:17 +0300

    testi
    testi

--- index.php
+++ index.php
@@ -10,7 +10,5 @@
 <?php echo '<p>Jenkins demo</p>'; ?>
 <?php echo '<p>Lisätty rivi 25.9</p>'; ?>
 <?php echo '<p>Juhan Demo</p>'; ?>
-<?php echo '<p>Jenkins demo</p>'; ?>
-<?php echo '<p>Terve janne, mitä kuuluu?</p>'; ?>
 </body>
 </html>
