d4c2e539dbbfa1b4251b59f01b442e15af01ffee
Mikko Honganmäki <42143818+mhonganm@users.noreply.github.com>
2018-10-14 15:55:39 +0300

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,6 @@
 <?php echo '<p>Jenkins demo</p>'; ?>
 <?php echo '<p>Lisätty rivi 25.9</p>'; ?>
 <?php echo '<p>Juhan Demo</p>'; ?>
-<?php echo '<p>test 14.10.</p>'; ?> 
+<?php echo '<p>testi 14.10.</p>'; ?> 
 </body>
 </html>
