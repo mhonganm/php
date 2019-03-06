9c364f568bdb6836cfc4569f0a126d89d06106b2
Mikko Honganmäki <42143818+mhonganm@users.noreply.github.com>
2019-03-06 11:45:33 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,18 +8,9 @@
 <?php echo '<h1 style="background-color:rgba(255, 99, 71, 0.5);font-family:verdana;text-align:center;">Heippa Maailma</h1>'; ?>
 <!--<?php echo '<h1>Heippa maailma</h1>'; ?>-->
   
-<?php echo '<p>mitä kuuluu</p>'; ?>
-<?php echo '<p>tämän pitäs nyt laukasta uusi versio</p>'; ?>
-<?php echo '<p>ja jälleen uusi versio</p>'; ?>
-<?php echo '<p>Jenkins demo</p>'; ?>
-<?php echo '<p>Lisätty rivi 25.9</p>'; ?>
-<?php echo '<p>Juhan Demo</p>'; ?>
-<?php echo '<p>Juhan Demo2</p>'; ?>
-<?php echo '<p>Juhan Demo3</p>'; ?>
-<?php echo '<p>nopea testi2 /mikko</p>'; ?>
-<?php echo '<p>nopea testi2 /mikko</p>'; ?>
-<?php echo '<p>nopea testi3 /mikko</p>'; ?>
-<?php echo '<p>gorn is a lizzard!!! </p>'; ?>
+<?php echo '<p>mitä kuuluu?</p>'; ?>
+<?php echo '<p style="text-align:right">Valtori pilvipäivä 14.3.</p>'; ?>
+<?php echo '<p>CI/CD demo jenkinsiä käyttäen</p>'; ?>
 <?php echo "The time is " . date("h:i:sa"); ?>
 </body>
 </html>
