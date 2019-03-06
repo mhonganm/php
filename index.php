6a7150823e9db0b412edff933469705718b5ca0b
Mikko Honganmäki <42143818+mhonganm@users.noreply.github.com>
2019-03-06 12:21:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,6 @@ l<html>
 <?php echo '<p style="text-align:right;font-size:160%;font-family:arial">Valtori pilvipäivä 14.3.</p>'; ?>
 <?php echo '<p style="text-align:left;font-size:200%;font-family:impact">CI/CD demo jenkinsiä käyttäen</p>'; ?>
 <?php echo '<p style="text-align:center;fon-size:120%:font-family:arial">Serverillä oleva aika  . date("h:i:sa")</p>'; ?>
-<?php date_default_timezone_set("Europe/Helsinki"); echo "Päivä ja aika: " . date("Y.m.d") . date("h:i:sa"); ?>
+<?php date_default_timezone_set("Europe/Helsinki"); echo "Päivä ja aika: " . date("Y.m.d") . " " . date("h:i:sa"); ?>
 </body>
 </html>
