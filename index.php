8e9fe9be519cc49348bb10f7ff0b58441cae7149
Mikko Honganmäki <42143818+mhonganm@users.noreply.github.com>
2019-03-06 17:21:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <title> PHP test - Jenkins - Openshift</title>
 </head>
 <!--<body>-->
-<body style="background-color:lemonchiffon;">
-  
+<!--<body style="background-color:lemonchiffon;">-->
+<body style="background-color:powderblue;">  
 <?php echo '<h1 style="background-color:rgba(255, 99, 71, 0.5);font-family:verdana;text-align:center;">Heippa Maailma</h1>'; ?>
 <!--<?php echo '<h1>Heippa maailma</h1>'; ?>-->
   
